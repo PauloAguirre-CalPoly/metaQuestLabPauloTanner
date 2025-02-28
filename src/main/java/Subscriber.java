2de57d72762bc226457a57cfe89543e7f0d7c5b7
PAguirre <paaguirr@calpoly.edu>
2025-02-27 17:09:14 -0800

    Update sub

--- src/main/java/Subscriber.java
+++ src/main/java/Subscriber.java
@@ -1,4 +1,5 @@
 import org.eclipse.paho.client.mqttv3.*;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -11,7 +12,13 @@ import java.io.IOException;
  * @version 1.0
  */
 public class Subscriber implements MqttCallback, Runnable {
-	
+
+	private float torsoX = 0.0f, torsoY = 1.4f, torsoZ = 0.0f;
+	private float leftHandX = -0.3f, leftHandY = 1.0f, leftHandZ = 0.5f;
+	private float rightHandX = 0.3f, rightHandY = 1.0f, rightHandZ = 0.5f;
+	private float leftGazeX = 0.0f, leftGazeY = 0.0f, leftGazeZ = 1.0f;
+	private float rightGazeX = 0.0f, rightGazeY = 0.0f, rightGazeZ = 1.0f;
+
 	private final static String BROKER = "tcp://test.mosquitto.org:1883";
 	private final static String TOPIC = "Lab5_Tanner_Paulo";
 	private final static String CLIENT_ID = "jgs-subscriber";
@@ -34,17 +41,42 @@ public class Subscriber implements MqttCallback, Runnable {
 
 	}
 
-//	public static void main(String[] args) throws MqttException {
-//
-//		MqttClient client = new MqttClient(BROKER, CLIENT_ID);
-//		Subscriber subscriber = new Subscriber();
-//		client.setCallback(subscriber);
-//		client.connect();
-//		System.out.println("Connected to BROKER: " + BROKER);
-//		client.subscribe(TOPIC);
-//		System.out.println("Subscribed to TOPIC: " + TOPIC);
-//
-//    }
+	private void parseData(String json) {
+		try {
+			JSONObject obj = new JSONObject(json);
+
+			JSONObject torso = obj.getJSONObject("torso");
+			torsoX = (float) torso.getDouble("x");
+			torsoY = (float) torso.getDouble("y");
+			torsoZ = (float) torso.getDouble("z");
+
+			// ✅ Parse hands and feet
+			JSONObject leftHand = obj.getJSONObject("leftHand");
+			leftHandX = (float) leftHand.getDouble("x");
+			leftHandY = (float) leftHand.getDouble("y");
+			JSONObject rightHand = obj.getJSONObject("rightHand");
+			rightHandX = (float) rightHand.getDouble("x");
+			rightHandY = (float) rightHand.getDouble("y");
+			JSONObject leftFoot = obj.getJSONObject("leftFoot");
+
+			// LeftGAZE
+			JSONObject leftEyeGaze = obj.getJSONObject("leftEyeGaze");
+			leftGazeX = (float) leftEyeGaze.getDouble("x");
+			leftGazeY = (float) leftEyeGaze.getDouble("y");
+			leftGazeZ = (float) leftEyeGaze.getDouble("z");
+			// Right GAZE
+			JSONObject rightEyeGaze = obj.getJSONObject("rightEyeGaze");
+			rightGazeX = (float) rightEyeGaze.getDouble("x");
+			rightGazeY = (float) rightEyeGaze.getDouble("y");
+			rightGazeZ = (float) rightEyeGaze.getDouble("z");
+			// ✅ Compute fixation point using both gaze rays
+			float[] leftGazeDir = {leftGazeX, leftGazeY, leftGazeZ};
+			float[] rightGazeDir = {rightGazeX, rightGazeY, rightGazeZ};
+
+		} catch (Exception e) {
+			System.out.println("Error parsing JSON: " + e.getMessage());
+		}
+	}
 	
 	@Override
 	public void connectionLost(Throwable throwable) {
@@ -55,7 +87,10 @@ public class Subscriber implements MqttCallback, Runnable {
 	public void messageArrived(String s, MqttMessage mqttMessage) throws IOException {
 		System.out.println("Message arrived. Topic: " + s +
 			" Message: " + new String(mqttMessage.getPayload()));
-		MotivData.getInstance().addData(new String(mqttMessage.getPayload()));
+		String payLoad = new String(mqttMessage.getPayload());
+		MotivData.getInstance().addData(payLoad);
+		parseData(payLoad);
+
 	}
 	
 	@Override
